626e740d1a7f92413eda1350c975698b8a579396
LAPTOP-MI64\Arunrobinson <aarunrobinson08@gmail.com>
2023-01-24 16:48:08 +0530

    commit

--- src/main/java/com/springapi/one/api_project/ApiProjectApplication.java
+++ src/main/java/com/springapi/one/api_project/ApiProjectApplication.java
@@ -2,12 +2,19 @@ package com.springapi.one.api_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApiProjectApplication {
+public class ApiProjectApplication   extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiProjectApplication.class, args);
 	}
+
+	@Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+      return builder.sources(ApiProjectApplication.class);
+  }
 
 }
